cadc1e9f65e5008c70f8b2f5f579638b19bc8e8c
Sean O'Reilly <seansoreilly@gmail.com>
2025-05-07 05:38:32 +1000

    initial commit

---
+++ src/my_project/agent.py
@@ -0,0 +1,16 @@
+def main(host, port):
+  capabilities = AgentCapabilities()
+  agent_card = AgentCard(
+    name="Echo Agent",
+    description="This agent echos the input given",
+    url=f"http://{host}:{port}/",
+    version="0.1.0",
+    defaultInputModes=["text"],
+    defaultOutputModes=["text"],
+    capabilities=capabilities,
+    skills=[skill]
+  )
+  logging.info(agent_card)
+
+if __name__ == "__main__":
+  main()
